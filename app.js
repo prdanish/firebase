1b01fd527aeceee1abfa4204e7c063aa4eaeab02
prdanish <dkrathore13579@gmail.com>
2023-06-20 14:45:39 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+
+
+import firebase from "firebase/compat/app";
+import "firebase/auth";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  // ...
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = firebase.auth();
+firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  });
+  
